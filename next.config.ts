fa8ec1c36727947b56567498b97a0385135daa80
ckarthik95-tech <c.karthik95@gmail.com>
2026-08-08 00:01:20 +0530

    Configure root domain export for custom domain ramesh.shawanreddy.com

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/OM-Sakthi-Real-Estate",
-  assetPrefix: "/OM-Sakthi-Real-Estate",
   images: {
     unoptimized: true,
   },
